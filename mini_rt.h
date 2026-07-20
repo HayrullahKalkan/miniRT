3dda53bf63babef01312f74ec8144258935634be
Hayrullah" Kalkan <hakalkan@k1m01s10.42istanbul.com.tr>
2026-07-20 22:27:04 +0300

    first commit

---
+++ mini_rt.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_rt.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakalkan <hakalkan@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 14:44:21 by hakalkan          #+#    #+#             */
+/*   Updated: 2026/07/20 22:26:16 by hakalkan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef MİNİ_RT_H
+#define MİNİ_RT_H
+
+#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "mlx.h"
+
+typedef struct  s_vektor
+{
+    int x;
+    int y;
+    int z;
+    
+}t_vektor;
+
+
+#endif
